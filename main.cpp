889da69f12373a2220c20e21a8702aed42abb1c1
GLgele <ai_minecraft@163.com>
2020-08-11 16:31:45 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,16 +4,43 @@ using namespace std;
 string sen = "欢迎来到这个MSDOS游戏!";
 int main()
 {
-    string temp;
-    printf("请输入玩家名!");
-    getline(cin,temp);
-    player p;
-    p.upName(temp);
-    system("cls");
-    for(int i=0;i<sen.length();i++)
-    {
-        cout<<sen[i];
-        Sleep(100);
-    }
-    return 0;
+	string temp;
+	int stat;
+	printf("请输入玩家名!\n");
+	getline(cin,temp);
+	player p;
+	p.upName(temp);
+	system("cls");
+	for(int i=0; i<sen.length(); i++)
+	{
+		cout<<sen[i];
+		Sleep(100);
+	}
+	system("cls");
+	while(true)
+	{
+		printf("\t欢迎来到DOS-RPG!\n");
+		printf("操作：\n1、随机领取任务\n2、任务中心\n0、退出\n");
+		scanf("%d",&stat);
+		if(stat == 1)
+		{
+
+		}
+		else if(stat == 2)
+		{
+
+		}
+		else if(stat == 0)
+		{
+			//保存文件
+			exit(0);
+		}
+		else
+		{
+			printf("错误的操作符！\n"); 
+			system("pause"); 
+			system("cls");
+		}
+	}
+	return 0;
 }
