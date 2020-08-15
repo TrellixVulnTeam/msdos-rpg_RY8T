bc354c4758fa081634e69ed76752080469b60d1e
GLgele <GLgele@outlook.com>
2020-08-15 14:03:05 +0800

    Create sprite.cpp

---
+++ cpp/sprite.cpp
@@ -0,0 +1,16 @@
+#include "../head/sprite.h"
+using namespace std;
+void sprite::upName(string _name)
+{
+    name = _name;
+}
+
+void sprite::upHealth(double _health)
+{
+    health = _health;
+}
+
+void sprite::upState(int _state)
+{
+    state = _state;
+}
